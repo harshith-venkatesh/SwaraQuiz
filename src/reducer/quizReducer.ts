f98e007c5916273a6840826f211f75df3d1647d5
Harshith7016 <harshithece7016@gmail.com>
2021-05-23 11:17:49 +0530

    feat: quiz reducer added

---
+++ src/reducer/quizReducer.ts
@@ -0,0 +1,96 @@
+import {
+  ActionType,
+  CALCULATE_SCORE,
+  CHOOSE_OPTION,
+  INCREMENT_QUESTION,
+  INITIALIZE_QUIZ,
+  InitialStateType,
+  RESET_QUIZ
+} from '../context/quizContext.type'
+import { isSelectedOptionRight } from '../utils/isSelectedOptionRight'
+
+export const initialState: InitialStateType = {
+  quizSelected: null,
+  currentQuestion: 1,
+  questionsAttempted: 0
+}
+
+export const quizReducer = (
+  state: InitialStateType,
+  action: ActionType
+): InitialStateType => {
+  switch (action.type) {
+    case INITIALIZE_QUIZ:
+      return {
+        ...state,
+        quizSelected: {
+          ...action.payload.quiz,
+          score: 0
+        }
+      }
+    case INCREMENT_QUESTION: {
+      return {
+        ...state,
+        currentQuestion: state.currentQuestion + 1
+      }
+    }
+    case CALCULATE_SCORE:
+      if (state.quizSelected) {
+        const score = state.quizSelected.questions.reduce(
+          (totalScore: number, { negetivePoints, points, options }): number =>
+            isSelectedOptionRight(options)
+              ? totalScore + points
+              : negetivePoints
+              ? totalScore - negetivePoints
+              : totalScore,
+          0
+        )
+        return {
+          ...state,
+          quizSelected: {
+            ...state.quizSelected,
+            score
+          }
+        }
+      } else {
+        throw new Error('No Quiz Selected')
+      }
+    case CHOOSE_OPTION:
+      if (state.quizSelected) {
+        const answeredQuestions = state.quizSelected.questions.map((question) =>
+          question._id === action.payload.questionId
+            ? {
+                ...question,
+                options: question.options.map((option) =>
+                  option._id === action.payload.optionId
+                    ? { ...option, isSelected: true }
+                    : { ...option, isSelected: false }
+                )
+              }
+            : question
+        )
+        const questionsAttemptedCheck = state.quizSelected.questions.some(
+          (question) => question._id === action.payload.questionId
+        )
+
+        return {
+          ...state,
+          quizSelected: {
+            ...state.quizSelected,
+            questions: answeredQuestions
+          },
+          questionsAttempted: questionsAttemptedCheck
+            ? state.questionsAttempted + 1
+            : state.questionsAttempted
+        }
+      } else {
+        throw new Error('No Quiz Selected')
+      }
+      case RESET_QUIZ:
+        return {
+          ...initialState
+        }
+    default:
+      return state
+  }
+}
